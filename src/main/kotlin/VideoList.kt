c7de5cfacdac71f82c580753e5730f2a704cec49
Nathaniel Mata <nathaniel@thepixelparty.com>
2021-10-03 13:22:02 -0700

    make react component interactive with onclick handler and state

--- src/main/kotlin/VideoList.kt
+++ src/main/kotlin/VideoList.kt
@@ -1,16 +1,32 @@
 import react.*
 import react.dom.*
+import kotlinx.browser.window
+import kotlinx.html.js.onClickFunction
 
 external interface VideoListProps: RProps {
     var videos: List<Video>
 }
 
+external interface VideoListState: RState {
+    var selectedVideo: Video?
+}
+
 @JsExport
-class VideoList: RComponent<VideoListProps, RState>() {
+class VideoList: RComponent<VideoListProps, VideoListState>() {
     override fun RBuilder.render() {
         for (video in props.videos) {
             p {
                 key = video.id.toString()
+                attrs {
+                    onClickFunction = {
+                        setState {
+                            selectedVideo = video
+                        }
+                    }
+                }
+                if (video == state.selectedVideo) {
+                    +"▶ "
+                }
                 +"${video.speaker}: ${video.title}"
             }
         }
